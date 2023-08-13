52f48573b7b079c4e5df8ceb75c52436fa6973f0
Elliot Akande <elbo.lynch@gmail.com>
2023-08-13 20:17:09 +0100

    Create gameBoard

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const playerFactory = (name, marker) => {
+const PlayerFactory = (name, marker) => {
     const _name = name;
     const _marker = marker;
 
@@ -10,3 +10,28 @@ const playerFactory = (name, marker) => {
         getMarker,
     };
 }
+
+const gameBoard = (() => {
+    const _board = [["", "", "",], 
+                    ["", "", "",], 
+                    ["", "", "",]];
+    
+    const play = (row, col, player) => {
+        if (_board[row][col] !== "") return;
+
+        _board[row][col] = player.getMarker();
+    };
+
+    const getBoard = () => _board;
+
+    const clear = () => {
+        _board.forEach(row => row.fill(""));
+    };
+
+    return {
+        play,
+        getBoard,
+        clear,
+    };
+})();
+
